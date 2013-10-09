a99bf3df7f8842224fd74053ae80911bf4ffb380
Trevor Fitzgerald <fitztrev@gmail.com>
2013-10-08 20:12:57 -0400

    use jekyll-utf8 plugin and simplify _plugins

--- _plugins/environment-variables.rb
+++
@@ -1,7 +0,0 @@
-module Jekyll
-	class EnvironmentVariablesGenerator < Generator
-		def generate(site)
-			site.config['is_ci'] = ENV['CI'] || false
-		end
-	end
-end
---
+++ _plugins/ext.rb
@@ -0,0 +1,4 @@
+require "jekyll-assets"
+require "jekyll-less"
+require 'jekyll-press'
+require 'jekyll-utf8'
